359cd92c6d366ddcbcfaab37762839652ed1bc33
Parkhi <h_w.park@samsung.com>
2018-01-03 13:29:17 +0900

    Add registry-notification package.
    Signed-off-by: Parkhi <h_w.park@samsung.com>

---
+++ src/api/registry/registryapi.go
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright 2017 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+
+// Package api/agent provides functionality to handle request related to agent.
+package registry
+
+import (
+	"api/common"
+	"commons/errors"
+	"commons/logger"
+	"commons/results"
+	URL "commons/url"
+	registrymanager "controller/management/registry"
+	"net/http"
+	"strings"
+)
+
+const (
+	GET    string = "GET"
+	PUT    string = "PUT"
+	POST   string = "POST"
+	DELETE string = "DELETE"
+)
+
+type RegistryAPIHandler struct{}
+type registryAPIExcutor struct{}
+
+var RegistryAPIHandle RegistryAPIHandlerInterface
+var registryAPI RegistryAPIInterface
+var registryManager registrymanager.RegistryInterface
+
+func init() {
+	RegistryAPIHandle = RegistryAPIHandler{}
+	registryAPI = registryAPIExcutor{}
+	registryManager = registrymanager.RegistryManager{}
+}
+
+// Handle calls a proper function according to the url and method received from remote device.
+func (RegistryAPIHandler) Handle(w http.ResponseWriter, req *http.Request) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	url := strings.Replace(req.URL.Path, URL.Base()+URL.Registry(), "", -1)
+	split := strings.Split(url, "/")
+
+	switch len(split) {
+	case 1:
+		if req.Method == GET {
+			registryAPI.getDockerRegistries(w, req)
+		} else if req.Method == POST {
+			registryAPI.registerDockerRegistry(w, req)
+		} else {
+			common.WriteError(w, errors.InvalidMethod{req.Method})
+		}
+
+	case 2:
+		if "/"+split[1] == URL.Events() {
+			if req.Method == POST {
+				registryAPI.handleDockerRegistryEvent(w, req)
+			} else {
+				common.WriteError(w, errors.InvalidMethod{req.Method})
+			}
+		} else {
+			if req.Method == GET {
+				registryID := split[1]
+				registryAPI.getDockerRegistry(w, req, registryID)
+			} else {
+				common.WriteError(w, errors.InvalidMethod{req.Method})
+			}
+		}
+	}
+}
+
+func (registryAPIExcutor) registerDockerRegistry(w http.ResponseWriter, req *http.Request) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	body, err := common.GetBodyFromReq(req)
+	if err != nil {
+		common.MakeResponse(w, results.ERROR, nil, err)
+		return
+	}
+	
+	result, resp, err := registryManager.AddDockerRegistry(body)
+
+	common.MakeResponse(w, result, common.ChangeToJson(resp), err)
+}
+
+func (registryAPIExcutor) getDockerRegistries(w http.ResponseWriter, req *http.Request) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+	
+	result, resp, err := registryManager.GetDockerRegistries()
+
+	common.MakeResponse(w, result, common.ChangeToJson(resp), err)
+}
+
+func (registryAPIExcutor) getDockerRegistry(w http.ResponseWriter, req *http.Request, registryID string) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+	
+	result, resp, err := registryManager.GetDockerRegistry(registryID)
+
+	common.MakeResponse(w, result, common.ChangeToJson(resp), err)
+}
+
+func (registryAPIExcutor) handleDockerRegistryEvent(w http.ResponseWriter, req *http.Request){
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+	
+	body, err := common.GetBodyFromReq(req)
+	if err != nil {
+		common.MakeResponse(w, results.ERROR, nil, err)
+		return
+	}
+	
+	result, err := registryManager.DockerRegistryEventHandler(body)
+
+	common.MakeResponse(w, result, common.ChangeToJson(nil), err)
+}
+
---
+++ src/controller/management/registry/registry.go
@@ -0,0 +1,348 @@
+/*******************************************************************************
+ * Copyright 2017 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+
+package registry
+
+import (
+	"bytes"
+	"commons/errors"
+	"commons/logger"
+	"commons/results"
+	URL "commons/url"
+	"db/modelinterface"
+	registryDB "db/mongo/model/registry"
+	"encoding/json"
+	"messenger"
+)
+
+const (
+	ID           = "id"
+	IP           = "ip"
+	TARGETINFO   = "target"
+	REQUESTINFO  = "request"
+	REGISTRIES   = "registries"
+	REGISTRY     = "registry"
+	IMAGES       = "images"
+	REPOSITORIES = "repositories"
+	HOST         = "host"
+	REPOSITORY   = "repository"
+	TAG          = "tag"
+	SIZE         = "size"
+	ACTION       = "action"
+	EVENTS       = "events"
+	TIMESTAMP    = "timestamp"
+	PUSH         = "push"
+	DELETE       = "delete"
+)
+
+type RegistryManager struct{}
+
+var dbManager modelinterface.RegistryInterface
+var httpRequester messenger.MessengerInterface
+
+func init() {
+	dbManager = registryDB.DBManager{}
+	httpRequester = messenger.NewMessenger()
+}
+
+func (RegistryManager) AddDockerRegistry(body string) (int, map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	reqBody, err := convertJsonToMap(body)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, nil, err
+	}
+
+	// Check the URL is valiadated or not with catalog API.
+	urls := makeRequestUrl(reqBody[IP].(string), URL.Catalog())
+
+	codes, respStr := httpRequester.SendHttpRequest("GET", urls)
+	respMap, err := convertRespToMap(respStr)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, nil, err
+	}
+
+	result := codes[0]
+	if isSuccessCode(result) {
+		// Add new registry to database with given url.
+		registry, err := dbManager.AddDockerRegistry(reqBody[IP].(string))
+		if err != nil {
+			logger.Logging(logger.ERROR, err.Error())
+			return results.ERROR, nil, err
+		}
+		
+		imagesList := respMap[REPOSITORIES]
+		
+		// if registry has repository, add the list of repository.
+		if len(imagesList.([]interface{})) > 0 {
+			imagesInfo := make([]map[string]interface{}, len(imagesList.([]interface{})))
+
+			for i, imageInfo := range imagesList.([]interface{}) {
+				imagesInfo[i][REPOSITORY] = imageInfo.(string)
+				imagesInfo[i][TAG] = ""
+				imagesInfo[i][SIZE] = ""
+				imagesInfo[i][ACTION] = ""
+				imagesInfo[i][TIMESTAMP] = ""
+			}
+
+			err = dbManager.AddDockerImages(registry[ID].(string), imagesInfo)
+			if err != nil {
+				logger.Logging(logger.ERROR, err.Error())
+				return results.ERROR, nil, err
+			}
+		}
+
+		res := make(map[string]interface{})
+		res[ID] = registry[ID]
+
+		return results.OK, nil, err
+	}
+
+	return result, nil, err
+}
+
+func (RegistryManager) DeleteDockerRegistry(registryId string) (int, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	// Delete registry specified by registryId parameter.
+	err := dbManager.DeleteDockerRegistry(registryId)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, err
+	}
+
+	return results.OK, err
+}
+
+func (RegistryManager) GetDockerRegistries() (int, map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	// Get all of registries list.
+	registries, err := dbManager.GetDockerRegistries()
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, nil, err
+	}
+
+	res := make(map[string]interface{})
+	res[REGISTRIES] = registries
+
+	return results.OK, res, err
+}
+
+func (RegistryManager) GetDockerRegistry(registryId string) (int, map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	// Get registry specified by registryId parameter.
+	registry, err := dbManager.GetDockerRegistry(registryId)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, nil, err
+	}
+
+	res := make(map[string]interface{})
+	res[REGISTRY] = registry
+
+	return results.OK, res, err
+}
+
+func (RegistryManager) GetDockerImages(registryId string) (int, map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	// Get all of images list on registry specified by registryId parameter.
+	images, err := dbManager.GetDockerImages(registryId)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, nil, err
+	}
+
+	res := make(map[string]interface{})
+	res[IMAGES] = images
+
+	return results.OK, res, err
+}
+
+func (RegistryManager) DockerRegistryEventHandler(body string) (int, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	convertedBody, err := convertJsonToMap(body)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return results.ERROR, err
+	}
+	events := convertedBody[EVENTS]
+
+	for _, eventInfo := range events.([]interface{}) {
+		parsedEvent := make(map[string]interface{})
+		parsedEvent, err = parseEvnetInfo(eventInfo.(map[string]interface{}))
+		if err != nil {
+			logger.Logging(logger.ERROR, err.Error())
+			return results.ERROR, err
+		}
+
+		switch parsedEvent[ACTION] {
+		case PUSH:
+			err := addDockerImage(parsedEvent)
+			if err != nil {
+				logger.Logging(logger.ERROR, err.Error())
+				return results.ERROR, err
+			}
+		case DELETE:
+			err := deleteDockerImage(parsedEvent)
+			if err != nil {
+				logger.Logging(logger.ERROR, err.Error())
+				return results.ERROR, err
+			}
+		}
+	}
+
+	return results.OK, nil
+}
+
+func addDockerImage(imageInfo map[string]interface{}) error {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	host, err := dbManager.GetDockerRegistry(imageInfo[HOST].(string))
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return err
+	}
+
+	delete(imageInfo, HOST)
+	images := make([]map[string]interface{}, 0)
+	images = append(images, imageInfo)
+
+	err = dbManager.AddDockerImages(host[ID].(string), images)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func deleteDockerImage(imageInfo map[string]interface{}) error {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	host, err := dbManager.GetDockerRegistry(imageInfo[HOST].(string))
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return err
+	}
+
+	delete(imageInfo, HOST)
+
+	err = dbManager.DeleteDockerImage(host[ID].(string), imageInfo)
+	if err != nil {
+		logger.Logging(logger.ERROR, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// convertRespToMap converts a response in the form of JSON data into a map.
+// If successful, this function returns an error as nil.
+// otherwise, an appropriate error will be returned.
+func convertRespToMap(respStr []string) (map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	resp, err := convertJsonToMap(respStr[0])
+	if err != nil {
+		logger.Logging(logger.ERROR, "Failed to convert response from string to map")
+		return nil, errors.InternalServerError{"Json Converting Failed"}
+	}
+	return resp, err
+}
+
+// convertJsonToMap converts JSON data into a map.
+// If successful, this function returns an error as nil.
+// otherwise, an appropriate error will be returned.
+func convertJsonToMap(jsonStr string) (map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	result := make(map[string]interface{})
+	err := json.Unmarshal([]byte(jsonStr), &result)
+	if err != nil {
+		return nil, errors.InvalidJSON{"Unmarshalling Failed"}
+	}
+	return result, err
+}
+
+// makeRequestUrl make a list of urls that can be used to send a http request.
+func makeRequestUrl(ip string, api_parts ...string) (urls []string) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	var httpTag string = "http://"
+	var full_url bytes.Buffer
+
+	full_url.Reset()
+	full_url.WriteString(httpTag + ip)
+
+	for _, api_part := range api_parts {
+		full_url.WriteString(api_part)
+	}
+	urls = append(urls, full_url.String())
+
+	return urls
+}
+
+// isSuccessCode returns true in case of success and false otherwise.
+func isSuccessCode(code int) bool {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	if code >= 200 && code <= 299 {
+		return true
+	}
+	return false
+}
+
+// parseEventInfo parse data which is matched image-info on DB from event-notification.
+func parseEvnetInfo(eventInfo map[string]interface{}) (map[string]interface{}, error) {
+	logger.Logging(logger.DEBUG, "IN")
+	defer logger.Logging(logger.DEBUG, "OUT")
+
+	targetInfoEvent := make(map[string]interface{})
+	requestInfoEvent := make(map[string]interface{})
+	parsedEvent := make(map[string]interface{})
+	
+	targetInfoEvent = eventInfo[TARGETINFO].(map[string]interface{})
+	requestInfoEvent = eventInfo[REQUESTINFO].(map[string]interface{})
+
+	parsedEvent[ACTION] = eventInfo[ACTION]
+	parsedEvent[TIMESTAMP] = eventInfo[TIMESTAMP]
+	parsedEvent[REPOSITORY] = targetInfoEvent[REPOSITORY]
+	parsedEvent[TAG] = targetInfoEvent[TAG]
+	parsedEvent[SIZE] = targetInfoEvent[SIZE]
+	parsedEvent[HOST] = requestInfoEvent[HOST]
+
+	return parsedEvent, nil
+}
