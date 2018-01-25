3ac276a0e802d3e53891d8f3fd8a3c0abfc6f7fc
Parkhi <h_w.park@samsung.com>
2018-01-25 16:07:36 +0900

    Add management/app package's mockfile and unittest.
    Signed-off-by: Parkhi <h_w.park@samsung.com>

---
+++ src/controller/management/app/appmanagement_test.go
@@ -0,0 +1,484 @@
+/*******************************************************************************
+ * Copyright 2017 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+package app
+
+import (
+	"commons/errors"
+	"commons/results"
+	dbmocks "db/mongo/app/mocks"
+	"github.com/golang/mock/gomock"
+	"reflect"
+	"testing"
+)
+
+const (
+	status  = "connected"
+	appId   = "000000000000000000000000"
+	nodeId = "000000000000000000000001"
+	ip      = "127.0.0.1"
+	port    = "48098"
+)
+
+var (
+	node = map[string]interface{}{
+		"id":     nodeId,
+		"ip":     ip,
+		"apps":   []string{},
+		"config": configuration,
+	}
+	address = []map[string]interface{}{
+		map[string]interface{}{
+			"ip": ip,
+		}}
+	configuration = map[string]interface{}{
+		"key": "value",
+	}
+	body             = `{"description":"description"}`
+	respCode         = []int{results.OK}
+	errorRespCode    = []int{results.ERROR}
+	respStr          = []string{`{"response":"response"}`}
+	invalidRespStr   = []string{`{"invalidJson"}`}
+	notFoundError    = errors.NotFound{}
+	connectionError  = errors.DBConnectionError{}
+	invalidJsonError = errors.InvalidJSON{}
+)
+
+var manager Command
+
+func init() {
+	manager = Executor{}
+}
+
+func TestCalledRegisterNodeWithValidBody_ExpectSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	body := `{"ip":"127.0.0.1", "config":{"key":"value"}}`
+	expectedRes := map[string]interface{}{
+		"id": "000000000000000000000001",
+	}
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().AddNode(ip, status, configuration).Return(node, nil),
+	)
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, res, err := manager.RegisterNode(body)
+
+	if err != nil {
+		t.Errorf("Unexpected err: %s", err.Error())
+	}
+
+	if code != results.OK {
+		t.Errorf("Expected code: %d, actual code: %d", results.OK, code)
+	}
+
+	if !reflect.DeepEqual(res, expectedRes) {
+		t.Error()
+	}
+}
+
+func TestCalledRegisterNodeWithInValidJsonFormatBody_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	invalidBody := `{"ip"}`
+
+	code, _, err := manager.RegisterNode(invalidBody)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", err.Error())
+	case errors.InvalidJSON:
+	}
+}
+
+func TestCalledRegisterNodeWithInvalidBodyNotIncludingIPField_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	invalidBody := `{"key":"value"}`
+
+	code, _, err := manager.RegisterNode(invalidBody)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", err.Error())
+	case errors.InvalidJSON:
+	}
+}
+
+func TestCalledRegisterNodeWhenFailedToInsertNewNodeToDB_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().AddNode(ip, status, configuration).Return(nil, notFoundError),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	body := `{"ip":"127.0.0.1", "config":{"key":"value"}}`
+	code, _, err := manager.RegisterNode(body)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", err.Error())
+	case errors.NotFound:
+	}
+}
+
+func TestCalledUnRegisterNodeWithValidBody_ExpectSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	expectedUrl := []string{"http://" + ip + ":" + port + "/api/v1/management/unregister"}
+
+	msgMockObj := msgmocks.NewMockCommand(ctrl)
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(node, nil),
+		msgMockObj.EXPECT().SendHttpRequest("POST", expectedUrl).Return(respCode, respStr),
+		dbExecutorMockObj.EXPECT().DeleteNode(nodeId).Return(nil),
+	)
+	// pass mockObj to a real object.
+	httpExecutor = msgMockObj
+	dbExecutor = dbExecutorMockObj
+
+	code, err := manager.UnRegisterNode(nodeId)
+
+	if code != results.OK {
+		t.Errorf("Expected code: %d, actual code: %d", results.OK, code)
+	}
+
+	if err != nil {
+		t.Errorf("Unexpected err: %s", err.Error())
+	}
+}
+
+func TestCalledUnRegisterNodeWhenDBHasNotMatchedNode_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(nil, notFoundError),
+	)
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, err := manager.UnRegisterNode(nodeId)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", err.Error())
+	case errors.NotFound:
+	}
+}
+
+func TestCalledGetNode_ExpectSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(node, nil),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, res, err := manager.GetNode(nodeId)
+
+	if err != nil {
+		t.Errorf("Unexpected err: %s", err.Error())
+	}
+
+	if code != results.OK {
+		t.Errorf("Expected code: %d, actual code: %d", results.OK, code)
+	}
+
+	if !reflect.DeepEqual(res, node) {
+		t.Error()
+	}
+}
+
+func TestCalledGetNodeWhenDBReturnsError_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(nil, notFoundError),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, _, err := manager.GetNode(nodeId)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", err.Error())
+	case errors.NotFound:
+	}
+}
+
+func TestCalledGetNodes_ExpectSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	nodes := []map[string]interface{}{node}
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNodes().Return(nodes, nil),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, res, err := manager.GetNodes()
+
+	if err != nil {
+		t.Errorf("Unexpected err: %s", err.Error())
+	}
+
+	if code != results.OK {
+		t.Errorf("Expected code: %d, actual code: %d", results.OK, code)
+	}
+
+	if !reflect.DeepEqual(res["nodes"].([]map[string]interface{}), nodes) {
+		t.Error()
+	}
+}
+
+func TestCalledGetNodesWhenDBReturnsError_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNodes().Return(nil, notFoundError),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, _, err := manager.GetNodes()
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", err.Error())
+	case errors.NotFound:
+	}
+}
+
+func TestCalledUpdateNodeStatus_ExpectSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().UpdateNodeStatus(nodeId, status).Return(nil),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	err := manager.UpdateNodeStatus(nodeId, status)
+
+	if err != nil {
+		t.Errorf("Unexpected err: %s", err.Error())
+	}
+}
+
+func TestCalledUpdateNodeStatusWhenDBReturnsError_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().UpdateNodeStatus(nodeId, status).Return(notFoundError),
+	)
+
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	err := manager.UpdateNodeStatus(nodeId, status)
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", err.Error())
+	case errors.NotFound:
+	}
+}
+
+func TestCalledPingNodeWhenDBHasNotMatchedNode_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(nil, notFoundError),
+	)
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	code, err := Executor{}.PingNode(nodeId, "")
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "NotFound", err.Error())
+	case errors.NotFound:
+	}
+}
+
+func TestCalledPingNodeWithInvalidBody_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(node, nil),
+	)
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	invalidKeyBody := `{"key":"value"}`
+	code, err := Executor{}.PingNode(nodeId, invalidKeyBody)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", err.Error())
+	case errors.InvalidJSON:
+	}
+}
+
+func TestCalledPingNodeWithInvalidValueBody_ExpectErrorReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dbExecutorMockObj := dbmocks.NewMockCommand(ctrl)
+
+	gomock.InOrder(
+		dbExecutorMockObj.EXPECT().GetNode(nodeId).Return(node, nil),
+	)
+	// pass mockObj to a real object.
+	dbExecutor = dbExecutorMockObj
+
+	invalidValueBody := `{"interval":"value"}`
+	code, err := Executor{}.PingNode(nodeId, invalidValueBody)
+
+	if code != results.ERROR {
+		t.Errorf("Expected code: %d, actual code: %d", results.ERROR, code)
+	}
+
+	if err == nil {
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", "nil")
+	}
+
+	switch err.(type) {
+	default:
+		t.Errorf("Expected err: %s, actual err: %s", "InvalidJSON", err.Error())
+	case errors.InvalidJSON:
+	}
+}
